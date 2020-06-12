fbbb42a17d04fddc48d27990e42cb7c57ad5ee1d
Ian Schwartz <ian.schwartz23@gmail.com>
2020-06-12 13:47:35 -0600

    added layout and ingredient components, builder container, prop type validation

---
+++ burger-builder-app/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import classes from "./BurgerIngredient.module.css";
+import PropTypes from "prop-types";
+
+class BurgerIngredient extends Component {
+  render() {
+    let ingredient = null;
+
+    switch (this.props.type) {
+      case "bread-bottom":
+        ingredient = <div className={classes.breadBottom}></div>;
+        break;
+      case "bread-top":
+        ingredient = (
+          <div className={classes.breadTop}>
+            <div className={classes.seeds1}></div>
+            <div className={classes.seeds2}></div>
+          </div>
+        );
+        break;
+      case "meat":
+        ingredient = <div className={classes.meat}></div>;
+        break;
+      case "cheese":
+        ingredient = <div className={classes.cheese}></div>;
+        break;
+      case "lettuce":
+        ingredient = <div className={classes.lettuce}></div>;
+        break;
+      case "bacon":
+        ingredient = <div className={classes.bacon}></div>;
+        break;
+      default:
+        ingredient = null;
+    }
+    return ingredient;
+  }
+}
+
+BurgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired
+};
+
+export default BurgerIngredient;
