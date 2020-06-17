9b6bf4062daabf3db1fe718beb4e92477086f718
Ian Schwartz <ian.schwartz23@gmail.com>
2020-06-17 13:18:02 -0700

    build controls

--- burger-builder-app/src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ burger-builder-app/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,12 +1,14 @@
 import React from "react";
-import classes from "*.module.css";
+import classes from "../BuildControl/BuildControl.module.css";
 
 const BuildControl = (props) => {
   return (
     <div className={classes.buildControl}>
       <div className={classes.label}>{props.label}</div>
       <button className={classes.less}>Less</button>
-      <button className={classes.more}>More</button>
+      <button className={classes.more} onClick={props.added}>
+        More
+      </button>
     </div>
   );
 };
--- burger-builder-app/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder-app/src/components/Burger/BuildControls/BuildControls.js
@@ -1,7 +1,26 @@
 import React from "react";
+import classes from "../BuildControls/BuildControls.module.css";
+import BuildControl from "./BuildControl/BuildControl";
+
+const controls = [
+  { label: "Bacon", type: "bacon" },
+  { label: "Lettuce", type: "lettuce" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" },
+];
 
 const BuildControls = (props) => {
-  return <div></div>;
+  return (
+    <div className={classes.buildControls}>
+      {controls.map((item) => (
+        <BuildControl
+          key={item.label}
+          label={item.label}
+          added={() => props.ingredientAdded(item.type)}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default BuildControls;
