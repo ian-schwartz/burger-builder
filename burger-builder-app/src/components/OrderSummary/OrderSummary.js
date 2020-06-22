f2b2e214efe214089e97f25bba89d9d5ed4bdec5
Ian Schwartz <ian.schwartz23@gmail.com>
2020-06-22 14:52:26 -0700

    implemented backdrop and custom button component

--- burger-builder-app/src/components/OrderSummary/OrderSummary.js
+++ burger-builder-app/src/components/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
+import Button from "../UI/Button/Button";
 
 const OrderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
@@ -17,6 +18,12 @@ const OrderSummary = (props) => {
       <p>A delicious burger with the following ingredients</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout?</p>
+      <Button btnType="danger" clicked={props.purchaseCancelled}>
+        Cancel
+      </Button>
+      <Button btnType="success" clicked={props.purchaseContinued}>
+        Continue
+      </Button>
     </Aux>
   );
 };
--- burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -69,7 +69,15 @@ class BurgerBuilder extends Component {
 
   purchaseHandler = () => {
     this.setState({ purchasing: true });
-  }
+  };
+
+  purchaseCancelHandler = () => {
+    this.setState({ purchasing: false });
+  };
+
+  purchaseContinueHandler = () => {
+    alert("Continue!");
+  };
 
   render() {
     const disabledInfo = {
@@ -81,8 +89,15 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
-          <OrderSummary ingredients={this.state.ingredients} />
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}
+        >
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}
+          />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
