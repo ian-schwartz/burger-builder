a65a439545f4c773847fe08fd3f9d6af3e27d26c
Ian Schwartz <ian.schwartz23@gmail.com>
2020-06-30 13:40:56 -0700

    improved performance using lifecycle methods

--- burger-builder-app/src/components/Burger/Burger.js
+++ burger-builder-app/src/components/Burger/Burger.js
@@ -16,7 +16,7 @@ const Burger = (props) => {
     transformedIngredients = <p>Please start adding ingredients!</p>;
   }
 
-  console.log(transformedIngredients);
+  // console.log(transformedIngredients);
 
   return (
     <div className={classes.burger}>
--- burger-builder-app/src/components/OrderSummary/OrderSummary.js
+++ burger-builder-app/src/components/OrderSummary/OrderSummary.js
@@ -1,34 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import Button from "../UI/Button/Button";
 
-const OrderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
-    return (
-      <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-        {props.ingredients[igKey]}
-      </li>
-    );
-  });
+class OrderSummary extends Component {
+  // Can change back to functional component
+  componentDidUpdate() {
+    console.log("[OrderSummary] did update");
+  }
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="danger" clicked={props.purchaseCancelled}>
-        Cancel
-      </Button>
-      <Button btnType="success" clicked={props.purchaseContinued}>
-        Continue
-      </Button>
-    </Aux>
-  );
-};
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKey) => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+            {this.props.ingredients[igKey]}
+          </li>
+        );
+      }
+    );
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="danger" clicked={this.props.purchaseCancelled}>
+          Cancel
+        </Button>
+        <Button btnType="success" clicked={this.props.purchaseContinued}>
+          Continue
+        </Button>
+      </Aux>
+    );
+  }
+}
 
 export default OrderSummary;
