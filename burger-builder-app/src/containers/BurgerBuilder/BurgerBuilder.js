f06538522ece9e3a0d123e72c8391b47ca1a1264
Ian Schwartz <ian.schwartz23@gmail.com>
2020-07-22 22:33:21 -0700

    created axios instance, post req

--- burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
   meat: 2,
@@ -76,7 +77,25 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("Continue!");
+    // alert("Continue!");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Ian Schwartz",
+        address: {
+          street: "Teststreet 1",
+          zipCode: "41251",
+          country: "US",
+        },
+        email: "test@example.com",
+      },
+      deliveryMethod: "fastest",
+    };
+    axios
+      .post("/orders.json", order)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
   };
 
   render() {
