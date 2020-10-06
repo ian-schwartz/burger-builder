0f0a1f09c6bc2c2e017e7e2ad7906c3874e5c517
Ian Schwartz <ian.schwartz23@gmail.com>
2020-10-05 23:39:00 -0700

    order handler

--- burger-builder-app/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder-app/src/containers/Checkout/ContactData/ContactData.js
@@ -12,16 +12,42 @@ class ContactData extends Component {
     },
   };
 
+  orderHandler = (event) => {
+    event.preventDefault();
+  };
+
   render() {
     return (
       <div className={classes.contactData}>
         <h4>Enter Your Contact Data</h4>
         <form>
-          <input className={classes.input} type="text" name="name" placeholder="Your Name"></input>
-          <input className={classes.input} type="email" name="email" placeholder="Your Email"></input>
-          <input className={classes.input} type="text" name="street" placeholder="Street"></input>
-          <input className={classes.input} type="text" name="postal" placeholder="Postal Code"></input>
-          <Button btnType="Success">ORDER</Button>
+          <input
+            className={classes.input}
+            type="text"
+            name="name"
+            placeholder="Your Name"
+          ></input>
+          <input
+            className={classes.input}
+            type="email"
+            name="email"
+            placeholder="Your Email"
+          ></input>
+          <input
+            className={classes.input}
+            type="text"
+            name="street"
+            placeholder="Street"
+          ></input>
+          <input
+            className={classes.input}
+            type="text"
+            name="postal"
+            placeholder="Postal Code"
+          ></input>
+          <Button btnType="Success" clicked={this.orderHandler}>
+            ORDER
+          </Button>
         </form>
       </div>
     );
